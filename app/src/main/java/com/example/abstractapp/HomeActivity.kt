2dbccaa55c177f5ed4d731f8a5c391aee5c10733
winnie.phebus <winn.phebus@gmail.com>
2022-01-04 15:22:33 -0500

    App Bar + Nav Drawer Working

--- app/src/main/java/com/example/abstractapp/HomeActivity.kt
+++ app/src/main/java/com/example/abstractapp/HomeActivity.kt
@@ -2,11 +2,25 @@ package com.example.abstractapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.abstractapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+
+        binding.navDrawer.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected TODO: properly figure out this routing eventually
+            menuItem.isChecked = true
+            binding.drawerLayout.close()
+            true
+        }
     }
 }
